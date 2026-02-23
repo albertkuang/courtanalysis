2dcb44a9c6ce11ae9a52af8e906e862b0a3f35c5
Albert Kuang <alberto.kuang@gmail.com>
2026-02-23 14:10:23 -0500

    Remove unnecessary files from repository

--- list_tables.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import pandas as pd
-
-conn = sqlite3.connect('tennis_data.db')
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print("Tables:", [row[0] for row in cursor.fetchall()])
-conn.close()
